74ace5efba6df0a27baafeef5a2916e5f97be8c7
Log-Wall <mrwallacelk@gmail.com>
2023-01-22 21:57:07 -0800

    npcs

---
+++ src/base/npcs.js
@@ -0,0 +1,48 @@
+export const npcs = [
+  {
+    npc: "a muscular angelic razer",
+    areaId: 123,
+    areaName: "Nur",
+    firstPerson:
+      /^Taking a brisk step backwards, a muscular angelic razer glares piercingly at you with unseeing eyes, his movements filled with contempt. Without warning, pain flares in your consciousness, radiating from your cranium to fill your body with rending agony\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: [],
+    length: 3.0,
+  },
+  {
+    npc: "a huge rat",
+    areaId: [338],
+    areaName: "the fathomless expanse of the World Tree",
+    firstPerson:
+      /^A huge rat snarls in fury as he bites down on your arm, sending a series of spasms racing through your body\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: ["damage", "loki", "loki"],
+    length: 4.0,
+  },
+  {
+    npc: "a huge rat",
+    areaId: [338],
+    areaName: "the fathomless expanse of the World Tree",
+    firstPerson:
+      /^Hissing with rage, a huge rat swipes a claw across your knees\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: ["damage", "loki"],
+    length: 4.0,
+  },
+  {
+    npc: "a huge rat",
+    areaId: [338],
+    areaName: "the fathomless expanse of the World Tree",
+    firstPerson:
+      /^Snarling as he leaps at your legs, a huge rat rips and tears into your flesh, his mouth filling with blood and saliva\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: ["damage", "loki", "torntendons", "clumsiness"],
+    length: 4.0,
+  },
+];
+//Hissing with rage, a huge rat swipes a claw across your knees. loki, damage
+//Snarling as he leaps at your legs, a huge rat rips and tears into your flesh, his mouth filling with blood and saliva. torntendons, damage
