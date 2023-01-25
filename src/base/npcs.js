5fc1f44abaef3922cc88f66821fb668f88416f0d
Log-Wall <mrwallacelk@gmail.com>
2023-01-24 22:27:45 -0800

    npcs

--- src/base/npcs.js
+++ src/base/npcs.js
@@ -22,6 +22,17 @@ export const npcs = [
     length: 4.0,
   },
   {
+    user: "a rajamala slaver",
+    areaId: [337],
+    areaName: "the fathomless expanse of the World Tree",
+    firstPerson:
+      /^A rajamala slaver leaps into the air and hurtles towards you\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: ["damage", "brokenleftleg", "brokenrightleg", "prone"],
+    length: 4.0,
+  },
+  {
     user: "a huge rat",
     areaId: [338],
     areaName: "the fathomless expanse of the World Tree",
@@ -144,6 +155,72 @@ export const npcs = [
     length: 2,
   },
   {
+    user: "a vanguard angel",
+    areaId: [401],
+    areaName: "First Circle of Nur",
+    firstPerson:
+      /^His luminous sword a blur of fiery strikes, a vanguard angel stabs you repeatedly, causing notable damage with each thrust\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: ["damage"],
+    length: 2,
+  },
+  {
+    user: "a vanguard angel",
+    areaId: [401],
+    areaName: "First Circle of Nur",
+    firstPerson:
+      /^With a snap, the angel's ethereal wings solidify into twin appendages of finely honed death\. He deploys them with a graceful pivot, mercilessly slicing all in their path\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: ["damage"],
+    length: 2,
+  },
+  {
+    user: "an expulsor angel",
+    areaId: [401],
+    areaName: "First Circle of Nur",
+    firstPerson:
+      /^An expulsor angel slams her buckler into your sternum with brutal force\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: ["damage"],
+    length: 2,
+  },
+  {
+    user: "an expulsor angel",
+    areaId: [401],
+    areaName: "First Circle of Nur",
+    firstPerson:
+      /^A single beat of her wings carries an expulsor angel briefly aloft before she dives towards you in a silent blur, driving her javelin deep into the juncture of your neck and shoulder\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: ["damage"],
+    length: 2,
+  },
+  {
+    user: "an expulsor angel",
+    areaId: [401],
+    areaName: "First Circle of Nur",
+    firstPerson:
+      /^Batting away your defences, an expulsor angel drives her javelin deep into your exposed body\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: ["damage"],
+    length: 2,
+  },
+  {
+    user: "an expulsor angel",
+    areaId: [401],
+    areaName: "First Circle of Nur",
+    firstPerson:
+      /^An expulsor angel slams her buckler into your sternum with brutal force\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: ["damage"],
+    length: 2,
+  },
+  {
     user: "an inquisitor angel",
     areaId: [401],
     areaName: "First Circle of Nur",
@@ -192,7 +269,7 @@ export const npcs = [
     areaId: [398],
     areaName: "Fifth Circle of Nur",
     firstPerson:
-      /^Closing with you briskly, a muscular angelic razer punches out with his sheathed sword, winding you with a hefty blow to the stomach. Stepping back unhurriedly, he pulls the sword from its scabbard and slashes at you twice in quick succession\.$/,
+      /^Closing with you briskly, a muscular angelic razer punches out with his sheathed sword, winding you with a hefty blow to the stomach\. Stepping back unhurriedly, he pulls the sword from its scabbard and slashes at you twice in quick succession\.$/,
     secondPerson: false,
     thirdPerson: false,
     tags: ["damage"],
@@ -203,18 +280,73 @@ export const npcs = [
     areaId: [398],
     areaName: "Fifth Circle of Nur",
     firstPerson:
-      /^A muscular angelic razer's wings flare powerfully, sending him colliding into you with his shoulder. The impact tosses you to the ground like a rag doll, and you can do naught but watch as his hands move in a blur, liberating from its sheath a bastard sword, and guiding it through the air to slice painfully into your exposed form\.$/,
+      /^A muscular angelic razer's wings flare powerfully, sending him colliding into you with his shoulder\. The impact tosses you to the ground like a rag doll, and you can do naught but watch as his hands move in a blur, liberating from its sheath a bastard sword, and guiding it through the air to slice painfully into your exposed form\.$/,
     secondPerson: false,
     thirdPerson: false,
     tags: ["damage", "prone"],
     length: 2,
   },
   {
+    user: "a lithe angelic razer",
+    areaId: [398],
+    areaName: "Fifth Circle of Nur",
+    firstPerson:
+      /^Taking a brisk step backwards, a lithe angelic razer glares piercingly at you with unseeing eyes, her movements filled with contempt\. Without warning, pain flares in your consciousness, radiating from your cranium to fill your body with rending agony\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: ["damage", "stupidity"],
+    length: 2,
+  },
+  {
+    user: "a lithe angelic razer",
+    areaId: [398],
+    areaName: "Fifth Circle of Nur",
+    firstPerson:
+      /^A lithe angelic razer abruptly blurs into motion, leaping through the air and slicing into you with her longswords\. An instant later, her full weight crashes into you as she plants one foot on your chest and kicks off, before floating to the ground a few paces away\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: ["damage"],
+    length: 2,
+  },
+  {
+    user: "a lithe angelic razer",
+    areaId: [398],
+    areaName: "Fifth Circle of Nur",
+    firstPerson:
+      /^Advancing upon you swiftly, a lithe angelic razer swings her left-hand longsword in a powerful overarm arc, inflicting a withering strike that crushes your bones\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: ["damage", "brokenleftarm"], //random ARM break
+    length: 2,
+  },
+  {
     user: "a solemn angel",
     areaId: [397],
     areaName: "Sixth Circle of Nur",
-    firstPerson: false,
-    //^\.$/,
+    firstPerson:
+      /^Looking down on you with primal ire, a solemn angel jerks her hand up, causing shafts of light to burst through the ground, impaling you in place\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: ["damage", "impale"],
+    length: 2,
+  },
+  {
+    user: "a solemn angel",
+    areaId: [397],
+    areaName: "Sixth Circle of Nur",
+    firstPerson:
+      /^Darting towards you with blinding speed, a solemn angel slashes her hand across your chest, leaving a ghastly glowing wound behind\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    tags: ["damage", "burning"],
+    length: 2,
+  },
+  {
+    user: "a solemn angel",
+    areaId: [397],
+    areaName: "Sixth Circle of Nur",
+    firstPerson:
+      /^Drawing her wings out to full length, a solemn angel swiftly claps them together, buffeting you with a massive gust of wind\.$/,
     secondPerson: false,
     thirdPerson: false,
     tags: ["damage", "prone"],
@@ -312,8 +444,7 @@ export const npcs = [
     user: "a burly troll guard",
     areaId: [260],
     areaName: "the Caverns of Riagath",
-    firstPerson:
-      /^A burly troll guard batters you with an overhead blow\.$/,
+    firstPerson: /^A burly troll guard batters you with an overhead blow\.$/,
     secondPerson: false,
     thirdPerson: false,
     tags: ["damage"],
