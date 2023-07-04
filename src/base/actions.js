c2219490b6cca1c54a81e7eeb4cff93e728e0e2a
Log-Wall <mrwallacelk@gmail.com>
2023-07-04 12:39:05 -0700

    depthswalker

--- src/base/actions.js
+++ src/base/actions.js
@@ -363,4 +363,33 @@ export const actions = [
     length: 2.2,
   },
   //#endregion
+  //#region Depthswalker
+  {
+    id: "reap",
+    fullName: "Reap",
+    firstPerson: false,
+    secondPerson: false,
+    thirdPerson:
+      /^(?<user>.+?) lashes out with a swift, reaping blow at (?<target>.+?)\.$/,
+    profession: ["depthswalker"],
+    skill: "shadowmancy",
+    balance: "balance",
+    multiLine: 1,
+    tags: [],
+    length: 2.2,
+  },
+  {
+    id: "instillleach",
+    fullName: "Instill - Leach",
+    firstPerson: false,
+    secondPerson: false,
+    thirdPerson:
+      /^As the weapon strikes (?<target>.+?), he seems greatly diminished\.$/,
+    profession: ["depthswalker"],
+    skill: "shadowmancy",
+    balance: "balance",
+    multiLine: 1,
+    tags: [], // parasite > healthleach > manaleach
+    length: 2.2,
+  },
 ];
