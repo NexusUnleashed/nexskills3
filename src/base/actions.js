e36d46eebae5e3a98a3d16d3a48fd5cef98af10b
Log-Wall <mrwallacelk@gmail.com>
2023-07-08 11:45:06 -0700

    psion

--- src/base/actions.js
+++ src/base/actions.js
@@ -389,7 +389,38 @@ export const actions = [
     skill: "shadowmancy",
     balance: "balance",
     multiLine: 1,
+    tags: [],
+    length: 2.2,
+  },
+  //#endregion
+  //#region Psion
+  {
+    id: "overhand",
+    fullName: "Overhand",
+    firstPerson: false,
+    secondPerson: false,
+    thirdPerson:
+      /^(?<user>.+?) brings a translucent mace around in a savage overhand strike, smashing it into the head of (?<target>.+?)\.$/,
+    profession: ["psion"],
+    skill: "weaving",
+    balance: "balance",
+    multiLine: 1,
     tags: [], // parasite > healthleach > manaleach
     length: 2.2,
   },
+  {
+    id: "deathblow",
+    fullName: "Deathblow",
+    firstPerson: false,
+    secondPerson: false,
+    thirdPerson:
+      /^Almost too swift to perceive, (?<user>.+?) lashes out with a translucent dagger, tracing a bloody line across the throat of (?<target>.+?)\.$/,
+    profession: ["psion"],
+    skill: "weaving",
+    balance: "balance",
+    multiLine: 1,
+    tags: ["asthma", "bleeding"], // parasite > healthleach > manaleach
+    length: 2.2,
+  },
+  //#endregion
 ];
