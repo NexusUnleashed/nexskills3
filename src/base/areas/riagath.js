516c8c084475414edacd525c107a946678f3a560
Logan Wallace <mrwallacelk@gmail.com>
2023-09-12 14:39:10 -0700

    breaking up into smaller code files

---
+++ src/base/areas/riagath.js
@@ -0,0 +1,145 @@
+export const npcs = [
+    {
+        user: "a gigantic angler fish",
+        areaId: [260],
+        areaName: "the Caverns of Riagath",
+        firstPerson:
+            /^A gigantic angler fish rapidly lunges towards you, its razor-sharp teeth snapping shut on your arm\.$/,
+        thirdPerson: false,
+        tags: ["damage"],
+        length: 2.0,
+    },
+    {
+        user: "a gigantic angler fish",
+        areaId: [260],
+        areaName: "the Caverns of Riagath",
+        firstPerson:
+            /^A gigantic angler fish throws itself at you, clamping its sickening jaw tight around your throat\.$/,
+        thirdPerson: false,
+        tags: ["damage", "blackout"],
+        length: 2.0,
+    },
+    {
+        user: "a gigantic angler fish",
+        areaId: [260],
+        areaName: "the Caverns of Riagath",
+        firstPerson:
+            /^A gigantic angler fish hurls itself at you in a vicious frontal attack, connecting painfully with your chest\.$/,
+        thirdPerson: false,
+        tags: ["damage", "stun"],
+        length: 2.0,
+    },
+    {
+        user: "a burly troll guard",
+        areaId: [260],
+        areaName: "the Caverns of Riagath",
+        firstPerson:
+            /^A burly troll guard lowers his head and charges at you, slamming powerfully into your abdomen\.$/,
+        thirdPerson: false,
+        tags: ["damage", "stun"],
+        length: 4.0,
+    },
+    {
+        user: "a burly troll guard",
+        areaId: [260],
+        areaName: "the Caverns of Riagath",
+        firstPerson:
+            /^A burly troll guard lowers his head and charges at you, slamming powerfully into your abdomen\.$/,
+        thirdPerson: false,
+        tags: ["damage", "stun"],
+        length: 4.0,
+    },
+    {
+        user: "a burly troll guard",
+        areaId: [260],
+        areaName: "the Caverns of Riagath",
+        firstPerson: /^A burly troll guard batters you with an overhead blow\.$/,
+        thirdPerson: false,
+        tags: ["damage"],
+        length: 4.0,
+    },
+    {
+        //TODO Identical pattern
+        user: "a burly troll guard",
+        areaId: [260],
+        areaName: "the Caverns of Riagath",
+        firstPerson:
+            /^A burly troll guard screams with rage and drives his spear deep into your chest\.$/,
+        thirdPerson: false,
+        tags: ["damage", "balance"],
+        length: 4.0,
+    },
+    {
+        //TODO Identical pattern
+        user: "a burly troll guard",
+        areaId: [260],
+        areaName: "the Caverns of Riagath",
+        firstPerson:
+            /^A look of absolute rage takes over a burly troll guard as he lunges at you, sinking his abnormally sharp teeth deep into your throat\.$/,
+        thirdPerson: false,
+        tags: ["damage"],
+        length: 4.0,
+    },
+    {
+        //TODO Identical pattern
+        user: "a sturdy troll woman",
+        areaId: [260],
+        areaName: "the Caverns of Riagath",
+        firstPerson:
+            /^A sturdy troll woman screams with rage and drives her spear deep into your chest\.$/,
+        thirdPerson: false,
+        tags: ["damage", "balance"],
+        length: 4.0,
+    },
+    {
+        //TODO Identical pattern
+        user: "a sturdy troll woman",
+        areaId: [260],
+        areaName: "the Caverns of Riagath",
+        firstPerson:
+            /^A look of absolute rage takes over a sturdy troll woman as she lunges at you, sinking her abnormally sharp teeth deep into your throat\.$/,
+        thirdPerson: false,
+        tags: ["damage"],
+        length: 4.0,
+    },
+    {
+        user: "a sturdy troll woman",
+        areaId: [260],
+        areaName: "the Caverns of Riagath",
+        firstPerson:
+            /^A sturdy troll woman lowers her head and charges at you, slamming powerfully into your abdomen\.$/,
+        thirdPerson: false,
+        tags: ["damage", "stun"],
+        length: 4.0,
+    },
+    {
+        user: "Rinaga",
+        areaId: [260],
+        areaName: "the Caverns of Riagath",
+        firstPerson:
+            /^His trusty shovel held firmly in both hands, Rinaga batters you powerfully around the head with it\.$/,
+        thirdPerson: false,
+        tags: ["damage"],
+        length: 3.0,
+    },
+    {
+        user: "Rinaga",
+        areaId: [260],
+        areaName: "the Caverns of Riagath",
+        firstPerson:
+            /^Rinaga swings a red-hot poker at you with considerable force, slamming it powerfully into the side of your head\.$/,
+        thirdPerson: false,
+        tags: ["damage"],
+        length: 3.0,
+    },
+    {
+        user: "Rinaga",
+        areaId: [260],
+        areaName: "the Caverns of Riagath",
+        firstPerson:
+            /^Using his shovel with outstanding accuracy, Rinaga scoops a pile of burning coals from his fire and launches them at you\.$/,
+        thirdPerson: false,
+        tags: ["damage", "burning"],
+        length: 3.0,
+    },
+];
---
+++ src/base/areas/tuar.js
@@ -0,0 +1,52 @@
+export const npcs = [
+    {
+        user: "a Nelbennir scout",
+        areaId: [207],
+        areaName: "the Island of Tuar",
+        firstPerson:
+            /^Drawing one long, bony hand from beneath his grey robes a Nelbennir scout grabs at you, seizing your shoulder and sending stabbing pain through your entire body\.$/,
+        thirdPerson: false,
+        tags: ["damage"],
+        length: 2,
+    },
+    {
+        user: "a Nelbennir scout",
+        areaId: [207],
+        areaName: "the Island of Tuar",
+        firstPerson:
+            /^As a Nelbennir scout focuses intently upon you, his round, bulbous eyes cloud over darkly\.$/,
+        thirdPerson: false,
+        tags: ["damage"],
+        length: 2,
+    },
+    {
+        user: "a Nelbennir dart-thrower",
+        areaId: [207],
+        areaName: "the Island of Tuar",
+        firstPerson:
+            /^With a swift flick of his supple wrist, a Nelbennir dart-thrower lets fly a tiny, whistling dart towards you\.$/,
+        thirdPerson: false,
+        tags: ["damage", "loki"],
+        length: 2,
+    },
+    {
+        user: "a Nelbennir dart-thrower",
+        areaId: [207],
+        areaName: "the Island of Tuar",
+        firstPerson:
+            /^A Nelbennir dart-thrower swiftly looses a handful of thin, narrow darts, sending their wickedly barbed heads deep into your soft flesh\.$/,
+        thirdPerson: false,
+        tags: ["damage"],
+        length: 2,
+    },
+    {
+        user: "a spotted pernicon",
+        areaId: [207],
+        areaName: "the Island of Tuar",
+        firstPerson:
+            /^A spotted pernicon rears up on its hind legs and snaps its body forwards, impaling you with its long, sickle-shaped mandibles\.$/,
+        thirdPerson: false,
+        tags: ["damage"],
+        length: 2,
+    },
+];
