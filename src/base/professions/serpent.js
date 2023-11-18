a473685d6196a31e23875a09df6ca31f826e49cc
Log-Wall <mrwallacelk@gmail.com>
2023-11-18 07:46:22 -0800

    serpent

--- src/base/professions/serpent.js
+++ src/base/professions/serpent.js
@@ -1,30 +1,32 @@
 export const actions = [
-    //#region Subterfuge
-    {
-        id: "garrote",
-        fullName: "Garrote",
-        firstPerson: false,
-        secondPerson: false,
-        thirdPerson: /^(?<user>\w+?) slips behind (?<target>.+?) and garrotes \w+ with \w+ whip\.$/,
-        profession: ["serpent"],
-        skill: "subterfuge",
-        balance: "balance",
-        tags: ["damage"],
-        length: 3.0,
-    },
-    //#endregion
-    //#region Venom
-    {
-        id: "garrote",
-        fullName: "Garrote",
-        firstPerson: false,
-        secondPerson: false,
-        thirdPerson: /^(?<user>\w+?) hunches \w+ shoulders and lets out a soft hiss\.$/,
-        profession: ["serpent"],
-        skill: "subterfuge",
-        balance: "balance",
-        tags: ["damage"],
-        length: 3.0,
-    },
-    //#endregion
+  //#region Subterfuge
+  {
+    id: "garrote",
+    fullName: "Garrote",
+    firstPerson: false,
+    secondPerson: false,
+    thirdPerson:
+      /^(?<user>\w+?) slips behind (?<target>.+?) and garrotes \w+ with \w+ whip\.$/,
+    profession: ["serpent"],
+    skill: "subterfuge",
+    balance: "balance",
+    tags: ["damage"],
+    length: 3.0,
+  },
+  //#endregion
+  //#region Venom
+  {
+    id: "shrugging",
+    fullName: "Shrugging",
+    firstPerson: false,
+    secondPerson: false,
+    thirdPerson:
+      /^(?<user>\w+?) hunches \w+ shoulders and lets out a soft hiss\.$/,
+    profession: ["serpent"],
+    skill: "venom",
+    balance: "free",
+    tags: ["cure"],
+    length: 0.0,
+  },
+  //#endregion
 ];
