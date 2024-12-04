60ede4acf563e31af4a41af5cbb0a52b0e0be8c2
Log-Wall <mrwallacelk@gmail.com>
2024-12-03 21:46:53 -0800

    0.6.2

--- src/base/skills/elementalism.js
+++ src/base/skills/elementalism.js
@@ -36,7 +36,7 @@ const elementalism = {
       /^You drain the heat from the air around your enemies, causing them to experience the cold of the abyss itself\.$/,
     secondPerson: false,
     thirdPerson:
-      /^(?<user>\w+) gestures grandly, and causes a deepfreeze to settle over his enemies\.$/,
+      /^(?<user>\w+) gestures grandly, and causes a deepfreeze to settle over \w+ enemies\.$/,
     profession: ["magi"],
     skill: "elementalism",
     balance: "equilibrium",
@@ -115,7 +115,7 @@ const elementalism = {
     secondPerson:
       /^A lash of fire comes speeding in from the (?<info>\w+), to painfully scorch your flesh\.$/,
     thirdPerson:
-      /^(?<user>\w+) casts a spell, and a lash of flame appears. With a gesture, \w+ sends it speeding off to the (?<info>\w+)\.$/,
+      /^(?<user>\w+) casts a spell, and a lash of flame appears\. With a gesture, \w+ sends it speeding off to the (?<info>\w+)\.$/,
     profession: ["magi"],
     skill: "elementalism",
     balance: "equilibrium",
@@ -263,11 +263,283 @@ const elementalism = {
     info: false,
     length: 2.3,
   },
+  resonanceAirMinor: {
+    id: "resonanceAirMinor",
+    fullName: "Air - Minor",
+    firstPerson: false,
+    secondPerson:
+      /^You clutch at your throat as an invisible noose snaps close about your neck\.$/,
+    thirdPerson:
+      /^(?<target>\w+) clutches at \w+ throat as he gasps for breath\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "free",
+    tags: [],
+    affs: [],
+    info: false,
+    length: 0,
+  },
+  resonanceAirModerate: {
+    id: "resonanceAirModerate",
+    fullName: "Air - Moderate",
+    firstPerson: false,
+    secondPerson:
+      /^A vicious wind rises, slashing air and grit flaying at your body and leaving you sensitive and raw\.$/,
+    thirdPerson:
+      /^A vicious wind rises, lashing and flaying at the body of (?<target>\w+) to leave \w+ sensitive and raw\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "free",
+    tags: [],
+    affs: [],
+    info: false,
+    length: 0,
+  },
+  resonanceAirMajor: {
+    id: "resonanceAirMajor",
+    fullName: "Air - Major",
+    firstPerson: false,
+    secondPerson:
+      /^Breath stutters in your breast, and weakness crawls into every limb\.$/,
+    thirdPerson:
+      /^A dreadful pallor overcomes the features of (?<target>\w+)\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "free",
+    tags: [],
+    affs: [],
+    info: false,
+    length: 0,
+  },
+  resonanceWaterMinor: {
+    id: "resonanceWaterMinor",
+    fullName: "Water - Minor",
+    firstPerson:
+      /^As you begin to tap your connection with the Elemental Plane of Water, you summon up a freezing wave to deluge (?<target>\w+)\.$/,
+    secondPerson:
+      /^A wave of freezing water materialises as if from nothing, crashing down on you\.$/,
+    thirdPerson:
+      /^A freezing wave of water materialises as if from nothing, crashing down upon (?<target>\w+)\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "free",
+    tags: [],
+    affs: [],
+    info: false,
+    length: 0,
+  },
+  resonanceWaterModerate: {
+    id: "resonanceWaterModerate",
+    fullName: "Water - Moderate",
+    firstPerson:
+      /^You summon up a flurry of icicles to slash at (?<target>\w+) in a shower of freezing daggers\.$/,
+    secondPerson:
+      /^A flurry of icicles materialise to slice at you in a hail of freezing daggers\.$/,
+    thirdPerson:
+      /^A flurry of freezing icicles materialise to slash at (?<target>\w+) in a shower of freezing daggers\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "free",
+    tags: [],
+    affs: [],
+    info: false,
+    length: 0,
+  },
+  resonanceWaterMajor: {
+    id: "resonanceWaterMajor",
+    fullName: "Water - Major",
+    firstPerson:
+      /^The might of Elemental Water suffusing you, you tear at the vital fluids which sustain the body of (?<target>\w+) with your will alone\.$/,
+    secondPerson:
+      /^(?<user>\w+) makes no motion or word, but a roiling nausea rolls through you, a terrible dizziness and rejection as bile fills the back of your throat\.$/,
+    thirdPerson:
+      /^(?<user>\w+) makes no motion or word, but (?<target>\w+) turns shockingly pale and staggers as if struck\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "free",
+    tags: [],
+    affs: [],
+    info: false,
+    length: 0,
+  },
+  resonanceEarthMinor: {
+    id: "resonanceEarthMinor",
+    fullName: "Earth - Minor",
+    firstPerson: false,
+    secondPerson:
+      /^A lance of pain radiates from your (?<limb>.+?) as it snaps beneath the ravages of some arcane force\.$/,
+    thirdPerson:
+      /^A loud crack emanates from the (?<limb>.+?) of (?<target>\w+)\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "free",
+    tags: [],
+    affs: [],
+    info: false,
+    length: 0,
+    reaction(action) {
+      action.info = action.limb;
+      action.affs = [`broken${action.limb.replace(" ", "")}`];
+    },
+  },
+  resonanceEarthModerate: {
+    id: "resonanceEarthModerate",
+    fullName: "Earth - Moderate",
+    firstPerson:
+      /^You bleed off residual power from your connection with the Elemental Plane of Earth, directing it in an unfocussed blast of arcane might at (?<target>\w+)\.$/,
+    secondPerson:
+      /^A wave of arcane force slams into you, your bones shuddering and your vision swimming\.$/,
+    thirdPerson: /^(?<target>\w+) buckles under an unseen blow\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "free",
+    tags: [],
+    affs: [],
+    info: false,
+    length: 0,
+  },
+  resonanceEarthMajor: {
+    id: "resonanceEarthMajor",
+    fullName: "Earth - Major",
+    firstPerson:
+      /^The might of Elemental Earth coursing through you, you smite (?<target>\w+) with the full force of your directed will\.$/,
+    secondPerson:
+      /^Your ribs give way under the force of a terrible invisible blow\.$/,
+    thirdPerson:
+      /^(?<target>\w+) hacks and coughs as a series of unpleasant cracks sound in grim staccato from \w+ chest\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "free",
+    tags: [],
+    affs: [],
+    info: false,
+    length: 0,
+  },
+  resonanceFireMinor: {
+    id: "resonanceFireMinor",
+    fullName: "Fire - Minor",
+    firstPerson: /^You send a spray of sparks to assault (?<target>\w+)\.$/,
+    secondPerson:
+      /^(?<user>\w+) sends a scattering of sparks to sear your flesh\.$/,
+    thirdPerson:
+      /^(?<user>\w+) sends a spray of sparks to sear the flesh of (?<target>\w+)\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "free",
+    tags: [],
+    affs: [],
+    info: false,
+    length: 0,
+  },
+  resonanceFireModerate: {
+    id: "resonanceFireModerate",
+    fullName: "Fire - Moderate",
+    firstPerson: /^You will that (?<target>\w+) should burn, and so he does\.$/,
+    secondPerson:
+      /^A wave of heat rolls over you from the direction of (?<user>\w+), and flames ignite upon your skin\.$/,
+    thirdPerson:
+      /^The air between (?<user>\w+) and (?<target>\w+) ripple with a haze of heat, and flames ignite upon the skin of \w+\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "free",
+    tags: [],
+    affs: [],
+    info: false,
+    length: 0,
+  },
+  resonanceFireMajor: {
+    id: "resonanceFireMajor",
+    fullName: "Fire - Major",
+    firstPerson:
+      /^The incandescent might of Elemental Fire filling you, you command (?<target>\w+) to burn from within\.$/,
+    secondPerson:
+      /^A searing spark ignites within your chest, the insidious heat spreading like wildfire and causing your skin to bubble and blister\.$/,
+    thirdPerson:
+      /^Blisters burst open across the body of (?<target>\w+) as \w+ skin glows as if lit from within\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "free",
+    tags: [],
+    affs: [],
+    info: false,
+    length: 0,
+  },
+  emanationFire: {
+    id: "emanationFire",
+    fullName: "Emanation",
+    firstPerson:
+      /^The might of elemental Fire filling you like a scorching river, you lift .+? to point at (?<target>\w+) and unleash a stream of searing flame\.$/,
+    secondPerson:
+      /^(?<user>\w+) lifts .+? to point at you and unleashes a stream of brilliant flame\.$/,
+    thirdPerson:
+      /^(?<user>\w+) lifts .+? to point at (?<target>\w+) and unleashes a stream of flame so brilliantly white that it sears your eyes to look upon in the instant before it strikes\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "equilibrium",
+    tags: [],
+    affs: [],
+    info: "Fire",
+    length: 2.4,
+  },
+  emanationAir: {
+    id: "emanationAir",
+    fullName: "Emanation",
+    firstPerson:
+      /^You sweep .+? over your head and a great wind rises, picking up (?<target>\w+) and dashing them violently about before casting them back to the ground\.$/,
+    secondPerson:
+      /^(?<user>\w+) sweeps .+? over \w+ head and a great wind rises, picking you up in an unnatural grasp and casting you violently about before hurling you back to the ground\.$/,
+    thirdPerson:
+      /^(?<user>\w+) sweeps .+? overhead and a great wind rises, picking up (?<target>\w+) and casting \w+ violently about before hurling \w+ back to the ground\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "equilibrium",
+    tags: [],
+    affs: [],
+    info: "Air",
+    length: 2.4,
+  },
+  emanationEarth: {
+    id: "emanationEarth",
+    fullName: "Emanation",
+    firstPerson:
+      /^The might of elemental Earth thrumming within your bones, you point .+? at (?<target>\w+) and an intangible wave of power strikes forth to smite \w+\.$/,
+    secondPerson:
+      /^(?<user>\w+) points .+? at you, and an intangible wave of something strikes forth to smite you\. Your bones convulse within your body, and a terrible sense of wrongness sweeps throughout your mortal shell\.$/,
+    thirdPerson:
+      /^(?<user>\w+) points .+? at (?<target>\w+), and an intangible wave of force strikes forth to smite \w+\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "equilibrium",
+    tags: [],
+    affs: [],
+    info: "Earth",
+    length: 2.4,
+  },
+  emanationWater: {
+    id: "emanationWater",
+    fullName: "Emanation",
+    firstPerson:
+      /^The might of elemental Water surging through your body, you make the slightest flick with .+?, and the hand of Sllshya descends as a freezing deluge to smash (?<target>\w+)\.$/,
+    secondPerson:
+      /^(?<user>\w+) makes the slightest flick with .+?, and a deluge of freezing water descends like the hand of some angered titan to smash you with a hammer colder than the oldest grave\.$/,
+    thirdPerson:
+      /^(?<user>\w+) makes the slightest flick with .+?, and like the hand of some angered titan a great wave of water descends to smash down upon (?<target>\w+)\.$/,
+    profession: ["magi"],
+    skill: "elementalism",
+    balance: "equilibrium",
+    tags: [],
+    affs: [],
+    info: "Water",
+    length: 2.4,
+  },
   //stormhammer: With Sylvan
 };
 
 export default Object.values(elementalism);
 /*
+You are no longer resonant with the Elemental Plane of Air.
+You are now minorly resonant with the Elemental Planes of Air and Fire.
+
 (Myrddin) points \w+ staff at you, and a bolt of lightning cascades out and roars, screaming, into your body.
 
 A bolt of lightning roars into Ogoun's body.
