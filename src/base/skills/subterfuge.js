7b3f02d1e4a0af3d5a79fa4f9fc150385a2e358c
Khaseem <mrwallacelk@gmail.com>
2025-10-01 20:27:47 -0700

    subterfuge

--- src/base/skills/subterfuge.js
+++ src/base/skills/subterfuge.js
@@ -12,6 +12,49 @@ export const subterfuge = {
     tags: ["pve", "damage"],
     length: 3.0,
   },
+  doublestab: {
+    id: "doublestab",
+    fullName: "Double Stab",
+    firstPerson: /^(?<user>\w+) quickly pricks you with \w+ dirk\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    profession: ["serpent"],
+    skill: "subterfuge",
+    balance: "balance",
+    tags: [],
+    length: 2.8,
+  },
+  flay: {
+    id: "flay",
+    fullName: "Flay",
+    firstPerson:
+      /^With fluid grace, (?<user>\w+) snaps \w+ wrist, bringing .+? back to crack viciously into you\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    profession: ["serpent"],
+    skill: "subterfuge",
+    balance: "balance",
+    tags: [],
+    length: 2.0,
+  },
+  lightwall: {
+    id: "lightwall",
+    fullName: "Lightwall",
+    firstPerson:
+      /^(?<user>\w+) forms a ball of light in \w+ palm and hurls it (?<dir>\w+)\.$/,
+    secondPerson: false,
+    thirdPerson: false,
+    profession: ["serpent"],
+    skill: "subterfuge",
+    balance: "balance",
+    tags: [],
+    length: 2.0,
+    reaction(action) {
+      action.info = action.dir;
+    },
+  },
 };
-
+//Thato forms a ball of light in her palm and hurls it southwards.
+//Your left foot explodes in white hot pain as an arrow slams into it.
+//With fluid grace, Thato snaps her wrist, bringing a serpentine, steel-tipped whip back to crack viciously into you.
 export default Object.values(subterfuge);
